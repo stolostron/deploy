1ba9c2d33447a375d144ade0fdce252ca77e8825
Tim Pouyer <tim.pouyer@gmail.com>
2020-03-19 23:47:07 -0400

    update uninstall scripts

--- multiclusterhub-operator/uninstall.sh
+++ multiclusterhub-operator/uninstall.sh
@@ -2,22 +2,19 @@
 
 oc project open-cluster-management
 for deployment in $(oc get Deployments -n open-cluster-management | cut -f 1 -d ' '); do oc delete Deployment $deployment -n open-cluster-management --ignore-not-found; done
-oc delete csv multiclusterhub-operator.v0.0.1 --ignore-not-found || true
-oc delete csv etcdoperator.v0.9.4 --ignore-not-found || true
 oc delete csv multicloud-operators-subscription.v0.1.2 --ignore-not-found || true
 oc delete csv multicluster-operators-subscription.v0.1.0 --ignore-not-found || true
-oc delete subscription multiclusterhub-operator || true
-oc delete subscription etcdoperator.v0.9.4 || true
-oc delete subscription multicloud-operators-subscription.v0.1.2 || true
-oc delete crd placementrules.apps.open-cluster-management.io || true
+oc delete subscription multiclusterhub-operator --ignore-not-found || true
+oc delete subscription etcdoperator.v0.9.4 --ignore-not-found || true
+oc delete subscription multicloud-operators-subscription.v0.1.2 --ignore-not-found || true
+oc delete crd placementrules.apps.open-cluster-management.io --ignore-not-found || true
 oc delete crd multiclusterhubs.operators.open-cluster-management.io --ignore-not-found || true
-oc delete service multicluster-operators-subscription || true
+oc delete service multicluster-operators-subscription --ignore-not-found || true
 oc delete crd channels.apps.open-cluster-management.io --ignore-not-found || true
 oc delete crd deployables.apps.open-cluster-management.io --ignore-not-found || true
 oc delete crd helmreleases.apps.open-cluster-management.io --ignore-not-found || true
 oc delete crd subscriptions.apps.open-cluster-management.io --ignore-not-found || true
 for secret in $(oc get Secret -n open-cluster-management | grep multicluster | cut -f 1 -d ' '); do oc delete Secret $secret -n open-cluster-management --ignore-not-found; done
-for secret in $(oc get Secret -n open-cluster-management | grep cert-manager | cut -f 1 -d ' '); do oc delete Secret $secret -n open-cluster-management --ignore-not-found; done
 for configmap in $(oc get configmap -n open-cluster-management | tail -n +2 | cut -f 1 -d ' '); do oc delete configmap $configmap -n open-cluster-management --ignore-not-found; done
 oc delete crd etcdbackups.etcd.database.coreos.com --ignore-not-found || true
 oc delete crd etcdclusters.etcd.database.coreos.com --ignore-not-found || true
--- multiclusterhub/uninstall.sh
+++ multiclusterhub/uninstall.sh
@@ -7,6 +7,7 @@ oc delete appsub --all || true
 oc delete apiservice clusterapi.io --ignore-not-found || true
 oc delete apiservice clusterregistry.k8s.io --ignore-not-found || true
 oc delete apiservice mcm.ibm.com --ignore-not-found || true
+oc delete apiservice mcm-apiserver --ignore-not-found || true
 oc delete apiservice v1beta1.webhook.certmanager.k8s.io --ignore-not-found || true
 for subscription in $(oc get subscriptions.apps.open-cluster-management.io -n open-cluster-management | tail -n +2 | cut -f 1 -d ' '); do oc delete subscriptions.apps.open-cluster-management.io $subscription -n open-cluster-management --wait=false; done
 for helmrelease in $(oc get helmreleases.apps.open-cluster-management.io -n open-cluster-management | tail -n +2 | cut -f 1 -d ' '); do oc patch helmreleases.apps.open-cluster-management.io $helmrelease -n open-cluster-management --type json -p '[{ "op": "remove", "path": "/metadata/finalizers" }]'; oc delete helmreleases.apps.open-cluster-management.io $helmrelease -n open-cluster-management; done
@@ -15,16 +16,24 @@ for configmap in $(oc get configmap -n open-cluster-management  | grep cert-mana
 for configmap in $(oc get configmap -n open-cluster-management | grep ingress-controller | cut -f 1 -d ' '); do oc delete configmap $configmap -n hive --ignore-not-found; done
 oc delete crd endpointconfigs.multicloud.ibm.com --ignore-not-found || true
 oc delete deploy -n hive hive-controllers --ignore-not-found || true
-oc delete deploy -n hive hiveadmission --ignore-not-found || true
+oc delete apiservice hiveadmission -n hive --ignore-not-found || true
 oc get crd | grep "hive" | awk '{ print $1 }' | xargs oc delete crd --wait=false
 oc delete csv hive -n hive $(oc get csv -n hive | tail -n +2 | cut -f 1 -d ' ') --ignore-not-found || true
 oc get clusterrole | grep "hive" | awk '{ print $1 }' | xargs oc delete clusterrole
 oc get clusterrolebindings | grep "hive" | awk '{ print $1 }' | xargs oc delete clusterrolebinding
 for configmap in $(oc get configmap -n hive | tail -n +2 | cut -f 1 -d ' '); do oc delete configmap $configmap -n hive --ignore-not-found; done
-for webhook in $(oc get validatingwebhookconfiguration | grep hive | cut -f 1 -d ' '); do oc delete validatingwebhookconfiguration $webhook --ignore-not-found; done
-for secret in $(oc get Secret -n hive | grep hive | cut -f 1 -d ' '); do oc delete Secret $secret -n hive --ignore-not-found; done
 for secret in $(oc get Secret -n open-cluster-management | grep search | cut -f 1 -d ' '); do oc delete Secret $secret -n hive --ignore-not-found; done
 for secret in $(oc get Secret -n open-cluster-management  | grep cert-manager | cut -f 1 -d ' '); do oc delete Secret $secret -n hive --ignore-not-found; done
+for secret in $(oc get Secret -n open-cluster-management | grep multicloud | cut -f 1 -d ' '); do oc delete Secret $secret -n open-cluster-management --ignore-not-found; done
+for secret in $(oc get Secret -n open-cluster-management | grep cert-manager | cut -f 1 -d ' '); do oc delete Secret $secret -n open-cluster-management --ignore-not-found; done
+for secret in $(oc get Secret -n open-cluster-management | grep kui | cut -f 1 -d ' '); do oc delete Secret $secret -n open-cluster-management --ignore-not-found; done
+for secret in $(oc get Secret -n open-cluster-management | grep search | cut -f 1 -d ' '); do oc delete Secret $secret -n open-cluster-management --ignore-not-found; done
+for secret in $(oc get Secret -n open-cluster-management | grep sh.helm.release | cut -f 1 -d ' '); do oc delete Secret $secret -n open-cluster-management --ignore-not-found; done
+for secret in $(oc get Secret -n open-cluster-management | grep topology| cut -f 1 -d ' '); do oc delete Secret $secret -n open-cluster-management --ignore-not-found; done
+for secret in $(oc get Secret -n open-cluster-management | grep console-chart | cut -f 1 -d ' '); do oc delete Secret $secret -n open-cluster-management --ignore-not-found; done
+for secret in $(oc get Secret -n open-cluster-management | grep aws | cut -f 1 -d ' '); do oc delete Secret $secret -n open-cluster-management --ignore-not-found; done
+for webhook in $(oc get validatingwebhookconfiguration | grep hive | cut -f 1 -d ' '); do oc delete validatingwebhookconfiguration $webhook --ignore-not-found; done
+for secret in $(oc get Secret -n hive | grep hive | cut -f 1 -d ' '); do oc delete Secret $secret -n hive --ignore-not-found; done
 for secret in $(oc get Secret -n hive | grep console | cut -f 1 -d ' '); do oc delete Secret $secret -n hive --ignore-not-found; done
 for secret in $(oc get Secret -n hive | grep kui | cut -f 1 -d ' '); do oc delete Secret $secret -n hive --ignore-not-found; done
 for secret in $(oc get Secret -n hive | grep management-ingress | cut -f 1 -d ' '); do oc delete Secret $secret -n hive --ignore-not-found; done
