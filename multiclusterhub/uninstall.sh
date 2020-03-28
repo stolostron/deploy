76a9474e4b05c37a297d45242de5be251f123e2a
Joshua Packer <jpacker@redhat.com>
2020-03-27 20:06:48 -0400

    updates (#26)
    - Disable some of the delete calls
    - Adjust the output formatting in start
    - Support multiple run parameters

--- multiclusterhub/uninstall.sh
+++ multiclusterhub/uninstall.sh
@@ -16,11 +16,17 @@ remove-apiservices
 # cluster deployment cleanup now being done by clean-clusters.sh
 # for deployment in $(oc get ClusterDeployment --all-namespaces | tail -n +2 | cut -f 1 -d ' '); do echo "Deleting managed cluster $deployment... this may take a few minutes."; oc delete ClusterDeployment $deployment -n $deployment; echo "done."; done
 for cluster in $(oc get Cluster --all-namespaces --ignore-not-found | tail -n +2 | cut -f 1 -d ' '); do oc delete Cluster $cluster && oc delete namespace $cluster --wait=false --ignore-not-found; done
+# Deletes all subscriptions in the system
 oc delete appsub --all --ignore-not-found || true
 oc delete crd endpointconfigs.multicloud.ibm.com --ignore-not-found || true
-for subscription in $(oc get subscriptions.apps.open-cluster-management.io | tail -n +2 | cut -f 1 -d ' '); do oc delete subscriptions.apps.open-cluster-management.io $subscription --wait=false --ignore-not-found; done
-for helmrelease in $(oc get helmreleases.apps.open-cluster-management.io | tail -n +2 | cut -f 1 -d ' '); do oc patch helmreleases.apps.open-cluster-management.io $helmrelease --type json -p '[{ "op": "remove", "path": "/metadata/finalizers" }]'; done
+
+#for subscription in $(oc get subscriptions.apps.open-cluster-management.io | tail -n +2 | cut -f 1 -d ' '); do oc delete subscriptions.apps.open-cluster-management.io $subscription --wait=false --ignore-not-found; done
+#for helmrelease in $(oc get helmreleases.apps.open-cluster-management.io | tail -n +2 | cut -f 1 -d ' '); do oc patch helmreleases.apps.open-cluster-management.io $helmrelease --type json -p '[{ "op": "remove", "path": "/metadata/finalizers" }]'; done
+
+#Run through twice, first time initiate all the deletes, 2nd time wait. This makes it more likely if the user runs the finalizer patch there will be NO orphans
+for helmrelease in $(oc get helmreleases.apps.open-cluster-management.io | tail -n +2 | cut -f 1 -d ' '); do oc delete helmreleases.apps.open-cluster-management.io $helmrelease --wait=false --ignore-not-found; done
 for helmrelease in $(oc get helmreleases.apps.open-cluster-management.io | tail -n +2 | cut -f 1 -d ' '); do oc delete helmreleases.apps.open-cluster-management.io $helmrelease --ignore-not-found; done
+
 for webhook in $(oc get validatingwebhookconfiguration | grep cert-manager | cut -f 1 -d ' '); do oc delete validatingwebhookconfiguration $webhook --ignore-not-found; done
 for configmap in $(oc get configmap  | grep cert-manager | cut -f 1 -d ' '); do oc delete configmap $configmap -n hive --ignore-not-found; done
 for configmap in $(oc get configmap | grep ingress-controller | cut -f 1 -d ' '); do oc delete configmap $configmap -n hive --ignore-not-found; done
@@ -66,6 +72,6 @@ for secret in $(oc get Secret -n hive | grep topology | cut -f 1 -d ' '); do oc 
 oc delete namespace hive --wait=false
 
 #Additonal cleanup
-oc delete crd userpreferences.console.acm.io --ignore-not-found || true
-oc delete ConsoleLink acm-console-link --ignore-not-found || true
-oc delete OAuthClient multicloudingress --ignore-not-found || true
+#oc delete crd userpreferences.console.acm.io --ignore-not-found || true
+#oc delete ConsoleLink acm-console-link --ignore-not-found || true
+#oc delete OAuthClient multicloudingress --ignore-not-found || true
