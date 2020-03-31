bc01340818c6239880051b2071ba511d0e029a1f
Jakob Gray <jakobgray@ibm.com>
2020-03-31 09:16:25 -0400

    Add webhook deletion; organize operator cleanup

--- multiclusterhub/uninstall.sh
+++ multiclusterhub/uninstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-oc project open-cluster-management
+# oc project open-cluster-management
 
 # cluster deployment cleanup now being done by clean-clusters.sh
 # for deployment in $(oc get ClusterDeployment --all-namespaces | tail -n +2 | cut -f 1 -d ' '); do echo "Deleting managed cluster $deployment... this may take a few minutes."; oc delete ClusterDeployment $deployment -n $deployment; echo "done."; done
@@ -10,19 +10,26 @@ for cluster in $(oc get Cluster --all-namespaces --ignore-not-found | tail -n +2
 echo "Wait until helmreleases are deleted..."
 until [[ $(kubectl get helmreleases.apps.open-cluster-management.io --output json | jq -j '.items | length') == "0" ]]; do sleep 2; done
 
+# Not seen on cluster
+for apiservice in $(oc get apiservice | grep clusterapi.io | cut -f 1 -d ' '); do oc delete apiservice $apiservice --ignore-not-found; done
+for secret in $(oc get Secret | grep aws | cut -f 1 -d ' '); do oc delete Secret $secret --ignore-not-found; done
+for policy in $(oc get policies.policy.mcm.ibm.com | tail -n +2 | cut -f 1 -d ' '); do oc patch policies.policy.mcm.ibm.com $policy --type json -p '[{ "op": "remove", "path": "/metadata/finalizers" }]'; oc delete policies.policy.mcm.ibm.com $policy --ignore-not-found; done
+
 # Issue https://github.com/open-cluster-management/backlog/issues/1286
 oc delete crd compliances.compliance.mcm.ibm.com --ignore-not-found || true
 oc delete crd policies.policy.mcm.ibm.com --ignore-not-found || true
 
 # Working on in https://github.com/open-cluster-management/backlog/issues/786
-for configmap in $(oc get configmap | grep ingress-controller | cut -f 1 -d ' '); do oc delete configmap $configmap -n hive --ignore-not-found; done
+for configmap in $(oc get configmap | grep ingress-controller | cut -f 1 -d ' '); do oc delete configmap $configmap --ignore-not-found; done
 
 # Working on in https://github.com/open-cluster-management/backlog/issues/787
 for secret in $(oc get Secret | grep cert-manager | cut -f 1 -d ' '); do oc delete Secret $secret --ignore-not-found; done
 
-# Not seen on cluster
-for apiservice in $(oc get apiservice | grep clusterapi.io | cut -f 1 -d ' '); do oc delete apiservice $apiservice --ignore-not-found; done
-for secret in $(oc get Secret | grep aws | cut -f 1 -d ' '); do oc delete Secret $secret --ignore-not-found; done
+# Issues pending
+oc delete validatingwebhookconfiguration multiclusterhub-operator-validating-webhook --ignore-not-found
+oc delete mutatingwebhookconfiguration multiclusterhub-operator-mutating-webhook --ignore-not-found
+oc delete mutatingwebhookconfiguration mcm-mutating-webhook --ignore-not-found
+
 
 # Hive cleanup
 oc get crd | grep "hive" | awk '{ print $1 }' | xargs oc delete crd --wait=false --ignore-not-found || true
