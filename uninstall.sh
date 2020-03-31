1fdd55ae8d25ccee65b132c6d2de6761f3313ff9
Jorge Padilla <jpadilla@us.ibm.com>
2020-03-31 14:35:00 -0400

    Display target cluster before uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "This script will uninstall Open Cluster Management from the current OpenShift target cluster:"
+oc cluster-info | head -n 1 | awk '{print $NF}'
+echo ""
+
 ./clean-clusters.sh
 
 kubectl delete -k multiclusterhub/ --ignore-not-found
