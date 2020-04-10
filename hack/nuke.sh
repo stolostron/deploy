24820945b193be89b4ef32d061c6c9b9e522597c
Tim Pouyer <tim.pouyer@gmail.com>
2020-04-10 08:11:09 -0400

    clean up managed cluster

--- hack/nuke.sh
+++ hack/nuke.sh
@@ -131,3 +131,43 @@ oc delete oauthclient multicloudingress
 oc delete crd endpointconfigs.multicloud.ibm.com
 oc delete clusterrole rcm-controller
 oc delete clusterrolebinding rcm-controller
+
+
+# if we are on a managed-cluster let's remove it's stuff too
+if [ -z "${OPERATOR_NAMESPACE}" ]; then
+	OPERATOR_NAMESPACE="multicluster-endpoint"
+fi
+
+# Delete all endpoints.multicloud.ibm.com
+kubectl delete endpoints.multicloud.ibm.com -n ${OPERATOR_NAMESPACE}  --all --timeout=60s
+
+# Delete Deployment
+kubectl delete deployment ibm-multicluster-endpoint-operator -n ${OPERATOR_NAMESPACE}
+
+# Force delete all component CRDs if they still exist
+component_crds=(
+	applicationmanagers.multicloud.ibm.com
+	certpoliciescontroller.multicloud.ibm.com
+	ciscontrollers.multicloud.ibm.com
+	connectionmanagers.multicloud.ibm.com
+	iampoliciescontroller.multicloud.ibm.com
+	policycontrollers.multicloud.ibm.com
+	searchcollectors.multicloud.ibm.com
+	serviceregistries.multicloud.ibm.com
+	workmanagers.multicloud.ibm.com
+	endpoints.multicloud.ibm.com
+)
+
+for crd in "${component_crds[@]}"; do
+	echo "force delete all CustomResourceDefination ${crd} resources..."
+	for resource in `kubectl get ${crd} -o name -n ${OPERATOR_NAMESPACE}`; do
+		echo "attempt to delete ${crd} resource ${resource}..."
+		kubectl delete ${resource} -n ${OPERATOR_NAMESPACE} --timeout=15s
+		echo "force remove ${crd} resource ${resource}..."
+		kubectl patch ${resource} -n ${OPERATOR_NAMESPACE} --type="json" -p '[{"op": "remove", "path":"/metadata/finalizers"}]'
+	done
+	echo "force delete all CustomResourceDefination ${crd} resources..."
+	kubectl delete crd ${crd}
+done
+
+kubectl delete namespace ${OPERATOR_NAMESPACE}
