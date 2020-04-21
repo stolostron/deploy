ea3bca20dd3d22f9dee23f7d76b6d02e98ffca92
Tim Pouyer <tim.pouyer@gmail.com>
2020-04-21 15:34:17 -0400

    uninstall should always succeed

--- multiclusterhub/uninstall.sh
+++ multiclusterhub/uninstall.sh
@@ -106,8 +106,8 @@ for rolebinding in $(oc get clusterrolebindings | grep hive | cut -f 1 -d ' '); 
 for webhook in $(oc get validatingwebhookconfiguration | grep hive | cut -f 1 -d ' '); do oc delete validatingwebhookconfiguration $webhook --ignore-not-found; done
 for configmap in $(oc get configmap -n hive | tail -n +2 | cut -f 1 -d ' '); do oc delete configmap $configmap -n hive --ignore-not-found; done
 for secret in $(oc get Secret -n hive | grep hive | cut -f 1 -d ' '); do oc delete Secret $secret -n hive --ignore-not-found; done
-oc delete hiveconfig hive
-oc delete namespace hive --wait=false
+oc delete hiveconfig hive --ignore-not-found
+oc delete namespace hive --wait=false --ignore-not-found
 
 #Additonal cleanup
 #oc delete crd userpreferences.console.acm.io --ignore-not-found || true
