61fc5a221258ae26e3ae3054002f2ecfb657e694
Hanqiu Zhang <31426239+hanqiuzh@users.noreply.github.com>
2020-07-09 11:21:28 -0400

    add offline managed cluster cleanup script (#124)
    * add offline managed cluster cleanup script
    
    * udpate cleanup-offline-managed-cluster.md

---
+++ hack/cleanup-managed-cluster.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+###############################################################################
+# Copyright (c) 2020 Red Hat, Inc.
+###############################################################################
+
+if [ -z "${OPERATOR_NAMESPACE}" ]; then
+	OPERATOR_NAMESPACE="open-cluster-management-agent-addon"
+fi
+
+if [ -z "${KLUSTERLET_NAMESPACE}" ]; then
+	KLUSTERLET_NAMESPACE="open-cluster-management-agent"
+fi
+
+KUBECTL=oc
+
+# Force delete klusterlet
+echo "attempt to delete klusterlet"
+${KUBECTL} delete klusterlet klusterlet --timeout=60s
+${KUBECTL} delete namespace ${KLUSTERLET_NAMESPACE} --wait=false
+echo "force removing klusterlet"
+${KUBECTL} patch klusterlet klusterlet --type="json" -p '[{"op": "remove", "path":"/metadata/finalizers"}]'
+
+# Force delete all component CRDs if they still exist
+component_crds=(
+	applicationmanagers.agent.open-cluster-management.io
+	certpolicycontrollers.agent.open-cluster-management.io
+	iampolicycontrollers.agent.open-cluster-management.io
+	policycontrollers.agent.open-cluster-management.io
+	searchcollectors.agent.open-cluster-management.io
+	workmanagers.agent.open-cluster-management.io
+)
+
+for crd in "${component_crds[@]}"; do
+	echo "force delete all CustomResourceDefination ${crd} resources..."
+	for resource in `${KUBECTL} get ${crd} -o name -n ${OPERATOR_NAMESPACE}`; do
+		echo "attempt to delete ${crd} resource ${resource}..."
+		${KUBECTL} delete ${resource} -n ${OPERATOR_NAMESPACE} --timeout=30s
+		echo "force remove ${crd} resource ${resource}..."
+		${KUBECTL} patch ${resource} -n ${OPERATOR_NAMESPACE} --type="json" -p '[{"op": "remove", "path":"/metadata/finalizers"}]'
+	done
+	echo "force delete all CustomResourceDefination ${crd} resources..."
+	${KUBECTL} delete crd ${crd}
+done
+
+${KUBECTL} delete namespace ${OPERATOR_NAMESPACE}
