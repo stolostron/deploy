419c796ca2c5ec2f82184abdbf150711ef72d35b
Hanqiu Zhang <hanzhang@redhat.com>
2020-07-21 16:11:42 -0400

    add wating & makesure we deleted managedclusters

--- clean-clusters.sh
+++ clean-clusters.sh
@@ -57,19 +57,49 @@ echo "Detaching imported clusters (rhacm 2.0+)"
 for clusterName in `oc get managedcluster --ignore-not-found | grep -v "NAME" | awk '{ print $1 }'`; do
     printf " Detaching cluster ${clusterName}\n  "
     if [ $CLEAN_RESOURCES ]; then
+        DELETE_MANAGEDCLUSTER=1
         oc delete managedcluster ${clusterName} --wait=false
-        printf "  "  #Spacing
-    fi
-done
-
-echo "Second pass cleaning, by klusterletaddonconfig"
-for clusterName in `oc get klusterletaddonconfig --all-namespaces --ignore-not-found | grep -v "NAMESPACE" | awk '{ print $1 }'`; do
-    printf " Removing addons on cluster ${clusterName}\n  "
-    if [ $CLEAN_RESOURCES ]; then
         oc -n ${clusterName} delete klusterletaddonconfig ${clusterName} --wait=false
         printf "  "  #Spacing
     fi
 done
+
+if [ $DELETE_MANAGEDCLUSTER ] ; then
+    echo "Wait 100 seconds"
+    sleep 100
+fi
+
+echo "Deleting manifestworks"
+for clusterName in `oc get managedcluster --ignore-not-found | grep -v "NAME" | awk '{ print $1 }'`; do
+    printf " Removing manifestworks in ${clusterName}\n  "
+    if [ $CLEAN_RESOURCES ]; then
+        oc delete manifestwork -n ${clusterName} --wait=false --all
+        printf "  "  #Spacing
+        oc delete lease -n ${clusterName} cluster-lease-${clusterName}
+        printf "  "  #Spacing
+        oc delete ns ${clusterName} --wait=false
+    fi
+done
+
+if [ $DELETE_MANAGEDCLUSTER ] ; then
+    echo "Wait 20 seconds"
+    sleep 20
+fi
+
+echo "Force deleting all resources"
+for clusterName in  `oc get managedcluster --ignore-not-found | grep -v "NAME" | awk '{ print $1 }'`; do
+    printf " Force removing all manifestwork, klusterletaddonconfig on cluster ${clusterName}\n  "
+    if [ $CLEAN_RESOURCES ]; then
+        oc get manifestwork -n  ${clusterName} | grep -v NAME | awk '{print $1}' | xargs -n1 oc patch manifestwork -n  ${clusterName} -p '{"metadata":{"finalizers":[]}}' --type=merge
+        printf "  "  #Spacing
+        oc patch klusterletaddonconfig -n ${clusterName} ${clusterName} -p '{"metadata":{"finalizers":[]}}' --type=merge
+        printf "  "  #Spacing
+        oc patch managedcluster -n ${clusterName} ${clusterName} -p '{"metadata":{"finalizers":[]}}' --type=merge
+    fi
+done
+sleep 5
+
+
 
 echo "Done!"
 
