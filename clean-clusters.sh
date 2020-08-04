661aa72e736732eef8b55bd2fe8b255ce3cf6b0f
Hanqiu Zhang <31426239+hanqiuzh@users.noreply.github.com>
2020-08-04 14:58:26 -0400

    update clean-clusters.sh to delete managedcluster  (#134)
    * update clean-clusters.sh to delete managedcluster & klusterletaddonconfig
    
    * add wating & makesure we deleted managedclusters
    
    * Update clean-clusters.sh
    
    correct one of the spacing

--- clean-clusters.sh
+++ clean-clusters.sh
@@ -32,7 +32,7 @@ for clusterName in `oc get clusterDeployments --all-namespaces --ignore-not-foun
     fi
 done
 
-echo "Detaching imported clusters"
+echo "Detaching imported clusters (rhacm 1.0)"
 for clusterName in `oc get clusters --all-namespaces --ignore-not-found | grep -v "NAMESPACE" | awk '{ print $1 }'`; do
     printf " Detaching cluster ${clusterName}\n  "
     if [ $CLEAN_RESOURCES ]; then
@@ -52,6 +52,54 @@ for clusterName in `oc get endpointconfig --all-namespaces --ignore-not-found | 
         oc delete namespace ${clusterName} --wait=false
     fi
 done
+
+echo "Detaching imported clusters (rhacm 2.0+)"
+for clusterName in `oc get managedcluster --ignore-not-found | grep -v "NAME" | awk '{ print $1 }'`; do
+    printf " Detaching cluster ${clusterName}\n  "
+    if [ $CLEAN_RESOURCES ]; then
+        DELETE_MANAGEDCLUSTER=1
+        oc delete managedcluster ${clusterName} --wait=false
+        printf "  "  #Spacing
+        oc -n ${clusterName} delete klusterletaddonconfig ${clusterName} --wait=false
+    fi
+done
+
+if [ $DELETE_MANAGEDCLUSTER ] ; then
+    echo "Wait 100 seconds"
+    sleep 100
+fi
+
+echo "Deleting manifestworks"
+for clusterName in `oc get managedcluster --ignore-not-found | grep -v "NAME" | awk '{ print $1 }'`; do
+    printf " Removing manifestworks in ${clusterName}\n  "
+    if [ $CLEAN_RESOURCES ]; then
+        oc delete manifestwork -n ${clusterName} --wait=false --all
+        printf "  "  #Spacing
+        oc delete lease -n ${clusterName} cluster-lease-${clusterName}
+        printf "  "  #Spacing
+        oc delete ns ${clusterName} --wait=false
+    fi
+done
+
+if [ $DELETE_MANAGEDCLUSTER ] ; then
+    echo "Wait 20 seconds"
+    sleep 20
+fi
+
+echo "Force deleting all resources"
+for clusterName in  `oc get managedcluster --ignore-not-found | grep -v "NAME" | awk '{ print $1 }'`; do
+    printf " Force removing all manifestwork, klusterletaddonconfig on cluster ${clusterName}\n  "
+    if [ $CLEAN_RESOURCES ]; then
+        oc get manifestwork -n  ${clusterName} | grep -v NAME | awk '{print $1}' | xargs -n1 oc patch manifestwork -n  ${clusterName} -p '{"metadata":{"finalizers":[]}}' --type=merge
+        printf "  "  #Spacing
+        oc patch klusterletaddonconfig -n ${clusterName} ${clusterName} -p '{"metadata":{"finalizers":[]}}' --type=merge
+        printf "  "  #Spacing
+        oc patch managedcluster -n ${clusterName} ${clusterName} -p '{"metadata":{"finalizers":[]}}' --type=merge
+    fi
+done
+sleep 5
+
+
 
 echo "Done!"
 
