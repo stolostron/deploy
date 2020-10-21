0c784da3d7a97689c378f99c423a608348b673e3
Kyl Bempah <kbempah@redhat.com>
2020-10-21 15:39:35 -0400

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -46,18 +46,40 @@ printf "\n"
 oc cluster-info | head -n 1 | awk '{print $NF}'
 printf "\n"
 
-./clean-clusters.sh "$args"
+if [ -z ${OCM_NAMESPACE+x} ]; 
+then 
+    echo "OCM_NAMESPACE must be set"
+    exit 1
+else 
+    echo "Cleaning up resources in namespace: $OCM_NAMESPACE"
+fi
+oc delete mch --all -n $OCM_NAMESPACE
+## Delete all helm charts in given namespace
+helm ls --namespace $OCM_NAMESPACE | cut -f 1 | tail -n +2 | xargs -n 1 helm delete --namespace $OCM_NAMESPACE
+oc delete apiservice v1.admission.cluster.open-cluster-management.io v1beta1.webhook.certmanager.k8s.io v1.admission.cluster.open-cluster-management.io v1.admission.work.open-cluster-management.io
+oc delete clusterimageset --all
+oc delete configmap -n $OCM_NAMESPACE cert-manager-controller cert-manager-cainjector-leader-election cert-manager-cainjector-leader-election-core
+oc delete consolelink acm-console-link
+oc delete crd klusterletaddonconfigs.agent.open-cluster-management.io placementbindings.policy.open-cluster-management.io policies.policy.open-cluster-management.io userpreferences.console.open-cluster-management.io
+oc delete mutatingwebhookconfiguration cert-manager-webhook-v1alpha1
+oc delete oauthclient multicloudingress
+oc delete rolebinding -n kube-system cert-manager-webhook-webhook-authentication-reader
+oc delete scc kui-proxy-scc
+oc delete validatingwebhookconfiguration cert-manager-webhook-v1alpha1
+exit 0
 
-kubectl delete -k multiclusterhub/
-echo "Sleeping for 200 seconds to allow resources to finalize ..."
-sleep 200
+#./clean-clusters.sh "$args"
 
-kubectl delete -k multicluster-hub-operator/
-./multicluster-hub-operator/uninstall.sh
+#kubectl delete -k multiclusterhub/
+#echo "Sleeping for 200 seconds to allow resources to finalize ..."
+#sleep 200
 
-kubectl delete -k acm-operator/
-./acm-operator/uninstall.sh
+#kubectl delete -k multicluster-hub-operator/
+#./multicluster-hub-operator/uninstall.sh
 
-kubectl delete -k community-subscriptions/
+#kubectl delete -k acm-operator/
+#./acm-operator/uninstall.sh
+
+#kubectl delete -k community-subscriptions/
 
 exit 0
