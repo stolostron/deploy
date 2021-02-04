360ede0e9501e905884ac84c74511dc191d73893
Chris Doan <cdoan@redhat.com>
2021-02-04 12:29:56 -0600

    2.1 appliedmanifestwork cleanup (#142)

--- hack/cleanup-managed-cluster.sh
+++ hack/cleanup-managed-cluster.sh
@@ -28,6 +28,7 @@ component_crds=(
 	policycontrollers.agent.open-cluster-management.io
 	searchcollectors.agent.open-cluster-management.io
 	workmanagers.agent.open-cluster-management.io
+	appliedmanifestworks.work.open-cluster-management.io
 )
 
 for crd in "${component_crds[@]}"; do
