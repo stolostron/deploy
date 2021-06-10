0932ba2ef4e94c2efc972f0532da005c1de54756
Kyl Bempah <kbempah@redhat.com>
2021-06-10 11:56:44 -0400

    Add a sleep between prereq and sub

--- start.sh
+++ start.sh
@@ -254,6 +254,9 @@ fi
 printf "\n##### Applying prerequisites\n"
 kubectl apply --openapi-patch=true -k prereqs/
 
+printf "\n#####Allow secrets time to propagate #####\n"
+sleep 60
+
 printf "\n##### Applying $OPERATOR_DIRECTORY subscription #####\n"
 kubectl apply -k $OPERATOR_DIRECTORY/
 
