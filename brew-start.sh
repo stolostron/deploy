1382a6f64875e70e526a125a33d4397e9441981f
Simon Baeumer <sbaumer@redhat.com>
2021-10-01 16:50:17 +0200

    Add  to brew-start.sh
    Signed-off-by: Simon Baeumer <sbaumer@redhat.com>

--- brew-start.sh
+++ brew-start.sh
@@ -7,7 +7,8 @@
 
 
 #-----BOOTSTRAP HELPERS-----#
-source dev/lib/helpers.sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "$DIR/lib/helpers.sh"
 
 
 #-----PREPARATION & VARS-----#
