40c089019471059c4726dd597764dcb842c03549
Leo Ochoa <lochoa@redhat.com>
2022-04-29 15:01:37 +0200

    Fix uninstall.sh for oc 4.10 versions

--- uninstall.sh
+++ uninstall.sh
@@ -36,7 +36,7 @@ fi
 
 VER=$(oc version | grep "Client Version:")
 
-if ! [[ $VER =~ .*[4-9]\.[3-9]\..* ]]; then
+if ! [[ $VER =~ .*[4-9]|10\.[3-9]\..* ]]; then
     echo "oc cli version 4.3 or greater required. Please visit https://access.redhat.com/downloads/content/290/ver=4.3/rhel---8/4.3.9/x86_64/product-software."
     exit 1
 fi
