1f3f8c7a7a8a6899a39e497272b80723ad57e491
Kyl Bempah <24903083+Kyl-Bempah@users.noreply.github.com>
2024-01-03 14:57:00 -0500

    Remove 2.5 conditionals
    Now that all active versions are 2.5 or later the conditionals are no longer needed

--- start.sh
+++ start.sh
@@ -228,20 +228,20 @@ fi
 
 
 # If 2.5.0 or higher, install MCE
-if [[ $DEFAULT_SNAPSHOT =~ v{0,1}[2-9]\.[5-9]+\.[0-9]+.* ]]; then
-    _MCE_IMAGE_NAME="mce-custom-registry"
-    if [[ ${CUSTOM_REGISTRY_REPO} == "quay.io/stolostron" ]]; then
-        _MCE_IMAGE_NAME="cmb-custom-registry"
-    fi
-
-    if [[ "$MCE_SNAPSHOT_CHOICE" == "UNSET" ]]; then
-        MCE_SNAPSHOT_CHOICE=${DEFAULT_SNAPSHOT}
-    fi
-
-    IMG="${CUSTOM_REGISTRY_REPO}/${_MCE_IMAGE_NAME}:${MCE_SNAPSHOT_CHOICE}" yq eval '.spec.image = env(IMG)' catalogsources/multicluster-engine.yaml > ${OPERATOR_DIRECTORY}/multicluster-engine.yaml
-    oc apply -f ${OPERATOR_DIRECTORY}/multicluster-engine.yaml
-    waitForPod "multiclusterengine-catalog" "" "openshift-marketplace"
+#if [[ $DEFAULT_SNAPSHOT =~ v{0,1}[2-9]\.[5-9]+\.[0-9]+.* ]]; then
+_MCE_IMAGE_NAME="mce-custom-registry"
+if [[ ${CUSTOM_REGISTRY_REPO} == "quay.io/stolostron" ]]; then
+    _MCE_IMAGE_NAME="cmb-custom-registry"
 fi
+
+if [[ "$MCE_SNAPSHOT_CHOICE" == "UNSET" ]]; then
+    MCE_SNAPSHOT_CHOICE=${DEFAULT_SNAPSHOT}
+fi
+
+IMG="${CUSTOM_REGISTRY_REPO}/${_MCE_IMAGE_NAME}:${MCE_SNAPSHOT_CHOICE}" yq eval '.spec.image = env(IMG)' catalogsources/multicluster-engine.yaml > ${OPERATOR_DIRECTORY}/multicluster-engine.yaml
+oc apply -f ${OPERATOR_DIRECTORY}/multicluster-engine.yaml
+waitForPod "multiclusterengine-catalog" "" "openshift-marketplace"
+#fi
 
 # Set the subscription channel if the variable wasn't defined as input, defaulted to snapshot-<release-version>
 if [ -z "$SUBSCRIPTION_CHANNEL" ]; then
@@ -391,28 +391,28 @@ if [[ " $@ " =~ " --watch " ]]; then
                     message="$(echo $component | jq -r '.message')";
                     printf "%-30s\t%-10s\t%-30s\t%-30s\n" "$status_item" "$component_status" "$type" "$reason"
                 done     
-                if [[ $DEFAULT_SNAPSHOT =~ v{0,1}[2-9]\.[5-9]+\.[0-9]+.* ]]; then 
-                    mce_find=$(oc get mce --all-namespaces 2>&1)
-                    if [[ $mce_find == *"No resources found"* ]]; then
-                        echo "Still waiting on MCE to be created"
-                    else
-                        component_list=$(oc get mce --all-namespaces -o json | jq -r '.items[].status.components') 2>/dev/null
-                        printf "\n"
-                        printf "MCE COMPONENTS"
-                        printf "\n"
-                        printf "%-30s\t%-10s\t%-30s\t%-30s\n" "COMPONENT" "STATUS" "TYPE" "REASON"
-                        size=$(echo $component_list | jq -r '. | length')
-                        for (( c=0; c<$size; c++ )) do
-                            component=$(echo $component_list | jq --argjson c $c '.[$c]')
-                            status_item="$(echo $component | jq -r '.name')";
-                            component_status="$(echo $component | jq -r '.status')";
-                            type="$(echo $component | jq -r '.type')";
-                            reason="$(echo $component | jq -r '.reason')";
-                            message="$(echo $component | jq -r '.message')";
-                            printf "%-30s\t%-10s\t%-30s\t%-30s\n" "$status_item" "$component_status" "$type" "$reason"
-                        done
-                    fi
+                #if [[ $DEFAULT_SNAPSHOT =~ v{0,1}[2-9]\.[5-9]+\.[0-9]+.* ]]; then 
+                mce_find=$(oc get mce --all-namespaces 2>&1)
+                if [[ $mce_find == *"No resources found"* ]]; then
+                    echo "Still waiting on MCE to be created"
+                else
+                    component_list=$(oc get mce --all-namespaces -o json | jq -r '.items[].status.components') 2>/dev/null
+                    printf "\n"
+                    printf "MCE COMPONENTS"
+                    printf "\n"
+                    printf "%-30s\t%-10s\t%-30s\t%-30s\n" "COMPONENT" "STATUS" "TYPE" "REASON"
+                    size=$(echo $component_list | jq -r '. | length')
+                    for (( c=0; c<$size; c++ )) do
+                        component=$(echo $component_list | jq --argjson c $c '.[$c]')
+                        status_item="$(echo $component | jq -r '.name')";
+                        component_status="$(echo $component | jq -r '.status')";
+                        type="$(echo $component | jq -r '.type')";
+                        reason="$(echo $component | jq -r '.reason')";
+                        message="$(echo $component | jq -r '.message')";
+                        printf "%-30s\t%-10s\t%-30s\t%-30s\n" "$status_item" "$component_status" "$type" "$reason"
+                    done
                 fi
+                #fi
             fi
             echo ""
             acc=$((acc+30))
